dac05dd84af2a55e21dd7427f1bf66480cd4a684
Arpit Sharma <arpitshrm07@gmail.com>
2018-10-03 22:19:37 -0700

    Day-1-V0.1
    Grab,spawn random- needs fixes with interaction and scene design

---
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/DestroyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyScript : MonoBehaviour {
+
+    public float destroyTime = 5.0f;
+
+   // private float rotateSpeed = 80.0f;
+
+	// Use this for initialization
+	void Start () {
+     Destroy(gameObject, destroyTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(Vector3.right * Time.deltaTime * rotateSpeed);
+      //  Destroy(gameObject, destroyTime);
+    }
+}
