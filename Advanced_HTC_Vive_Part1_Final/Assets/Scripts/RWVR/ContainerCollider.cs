ea6aeee5ec8e078353921b7bf5f3d687ab25c02b
Arpit Sharma <arpitshrm07@gmail.com>
2018-10-10 19:58:44 -0700

    Collider Patch

--- Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerCollider.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerCollider.cs
@@ -4,19 +4,11 @@ using UnityEngine;
 
 public class ContainerCollider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-
-    }
     private void OnCollisionEnter(Collision collision)
     {
-       if (collision.gameObject.name == this.gameObject.name)
+        Debug.Log("Destroying after touch");
+
+        if (collision.gameObject.name == gameObject.tag)
         {
             Destroy(collision.gameObject);
             Debug.Log("Destroying after touch");
--- Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerGenerator.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerGenerator.cs
@@ -19,7 +19,7 @@ public class ContainerGenerator : MonoBehaviour {
 
         SendInputString ParentInputObject = Parent.GetComponent<SendInputString>();
         inputChild = ParentInputObject.input;
-
+        Parent.SetActive(false);
         //SendInputString.input = "";
 
         length = inputChild.Length;
@@ -37,11 +37,11 @@ public class ContainerGenerator : MonoBehaviour {
         {
             Vector3 temp = transform.position;
             Quaternion rotat = transform.rotation;
-            temp.z += 0.2f*(count+1);
+            temp.z += 0.25f*(count+1);
             //temp.y = 2;
             myTag = inputChild[count].ToString();
 
-            container.name = this.myTag;
+            container.tag = this.myTag;
             Instantiate(container, temp, rotat);
         
             count++;
