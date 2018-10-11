8331dac93b6c3fd7f853a5d041cde9e4a8b122c0
rahullele <rlele@usc.edu>
2018-10-10 23:44:10 -0700

    squat

---
+++ Advanced_HTC_Vive_Part1_Final/Assets/squat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class squat : MonoBehaviour {
+
+
+	// Use this for initialization
+	
+
+    void OnTriggerEnter(Collider other)
+    {
+            
+        GetComponent<AudioSource>().Play();
+            Debug.Log("Object entered the Trigger");
+            //GetComponent<AudioSource>().Play();
+
+
+    }
+}
