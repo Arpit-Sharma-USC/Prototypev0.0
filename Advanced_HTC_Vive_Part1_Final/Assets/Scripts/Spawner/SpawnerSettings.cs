231547521d2d34cb294d40cd2613f883e5d58ef0
vyasvarun95 <39752978+vyasvarun95@users.noreply.github.com>
2018-10-11 13:04:06 -0700

    Spawner wip

--- Advanced_HTC_Vive_Part1_Final/Assets/Scripts/Spawner/SpawnerSettings.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/Spawner/SpawnerSettings.cs
@@ -21,9 +21,11 @@ public class SpawnerSettings : MonoBehaviour
     //public int noOfSpawnedObjects = 0;
     public bool spawnAtMultiplePlaces = false;
     public float spawnIntensity = 50;
-
+    public float spawnInterval = 0;
     public GameObject[] gameObjects;
     //public ArrayList<GameObject> 
+
+    float time=0;
 
     // Use this for initialization
     void Start()
@@ -48,7 +50,7 @@ public class SpawnerSettings : MonoBehaviour
                     for (int i = 0; i < noOfRows; i++)
                         for (int j = 0; j < noOfColumns; j++)
                         {
-                            Debug.Log("in spawner!");
+                            //Debug.Log("in spawner!");
                             Vector3 spawnPos = transform.position;
                             spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
                             spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
@@ -56,13 +58,78 @@ public class SpawnerSettings : MonoBehaviour
                             {
                                 int randGOindex = Random.Range(0, gameObjects.Length);
                                 spawnPos.y += gameObjects[randGOindex].GetComponent<Renderer>().bounds.size.y / 2;
-                                Instantiate(gameObjects[randGOindex], spawnPos, Quaternion.identity);
-                                Debug.Log("Spawning!");
+                                GameObject spawnedGO = Instantiate(gameObjects[randGOindex], spawnPos, Quaternion.identity);
+                                spawnedGO.transform.parent = transform;
+                               // Debug.Log("Spawning!");
                             }
                         }
                 }
                 else
                 {
+                    List<Vector2> existingObjs = new List<Vector2>();
+
+                    for (int g = 0; g < gameObjects.Length; g++)
+                    {
+                        Debug.Log("Obj: " + g);
+                        float randIntensity = Random.Range(0.0f, 100.0f);
+                        Debug.Log("SpawnIntensity: " + randIntensity);
+                        if (randIntensity < spawnIntensity)
+                        {
+                            Debug.Log("Entering.");
+                            int randx = Random.Range(0, noOfRows);
+                            int randz = Random.Range(0, noOfColumns);
+                            Debug.Log("Location generated: " + randx + "," + randz);
+                            if(existingObjs.Contains(new Vector2(randx, randz)))
+                                //The grid still has spots.
+                                if(existingObjs.Count <= (noOfRows * noOfColumns))
+                                {
+                                    Debug.Log("Location not free!");
+                                    bool foundSpot = false;
+                                    for(int i = randx+1; i < noOfRows; i++)
+                                        for(int j = randz; j  <noOfColumns; j++)
+                                            if(!existingObjs.Contains(new Vector2(i, j)))
+                                            {
+                                                Vector3 spawnPos = transform.position;
+                                                spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                                spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                                spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                                GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                                spawnedGO.transform.parent = transform;
+                                                existingObjs.Add(new Vector2(i, j));
+                                                foundSpot = true;
+                                            }
+                                    if(!foundSpot)
+                                        for (int i = 0; i < randx; i++)
+                                            for (int j = 0; j < randz; j++)
+                                                if (!existingObjs.Contains(new Vector2(i, j)))
+                                                {
+                                                    Vector3 spawnPos = transform.position;
+                                                    spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                                    spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                                    spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                                    GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                                    spawnedGO.transform.parent = transform;
+                                                    existingObjs.Add(new Vector2(i, j));
+                                                }
+                                }
+
+                            
+                            
+                            else{
+                                    Debug.Log("Location free!");
+                                    Vector3 spawnPos = transform.position;
+                                    spawnPos.x += randx * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                    spawnPos.z += randz * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                    spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                    GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                    spawnedGO.transform.parent = transform;
+                                    existingObjs.Add(new Vector2(randx, randz));
+
+                                }
+                        }
+                        
+
+                    }
 
                 }
             else
@@ -77,7 +144,7 @@ public class SpawnerSettings : MonoBehaviour
         //spawnerInfo.noOfRows = noOfRows;
         //spawnerInfo.spacing = spacing;
 
-        //Editor Interface.
+        
         if (Application.isEditor && !Application.isPlaying)
         {
             while (transform.childCount != 0)
@@ -96,9 +163,100 @@ public class SpawnerSettings : MonoBehaviour
                 }
         }
         //if (Application.isPlaying)
-        if (!Application.isEditor && Application.isPlaying)
+        if (Application.isPlaying)
         {
+            time += Time.deltaTime;
+            if(time >= spawnInterval)
+            {
+                time = 0;
+                foreach (Transform child in transform)
+                {
+                    GameObject.Destroy(child.gameObject);
+                }
+                if (gameObjects.Length > 0)
+                    if (spawnAtMultiplePlaces)
+                    {
+                        for (int i = 0; i < noOfRows; i++)
+                            for (int j = 0; j < noOfColumns; j++)
+                            {
+                                Debug.Log("in spawner!");
+                                Vector3 spawnPos = transform.position;
+                                spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                if (Random.Range(0.0f, 100.0f) < spawnIntensity)
+                                {
+                                    int randGOindex = Random.Range(0, gameObjects.Length);
+                                    spawnPos.y += gameObjects[randGOindex].GetComponent<Renderer>().bounds.size.y / 2;
+                                    GameObject spawnedGO = Instantiate(gameObjects[randGOindex], spawnPos, Quaternion.identity);
+                                    spawnedGO.transform.parent = transform;
+                                    Debug.Log("Spawning!");
+                                }
+                            }
+                    }
+                    else
+                    {
+                        List<Vector2> existingObjs = new List<Vector2>();
 
+                        for (int g = 0; g < gameObjects.Length; g++)
+                        {
+
+                            if (Random.Range(0.0f, 100.0f) < spawnIntensity)
+                            {
+                                int randx = Random.Range(0, noOfRows);
+                                int randz = Random.Range(0, noOfColumns);
+                                if (existingObjs.Contains(new Vector2(randx, randz)))
+                                    //The grid still has spots.
+                                    if (existingObjs.Count <= (noOfRows * noOfColumns))
+                                    {
+                                        bool foundSpot = false;
+                                        for (int i = randx + 1; i < noOfRows; i++)
+                                            for (int j = randz; j < noOfColumns; j++)
+                                                if (!existingObjs.Contains(new Vector2(i, j)))
+                                                {
+                                                    Vector3 spawnPos = transform.position;
+                                                    spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                                    spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                                    spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                                    GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                                    spawnedGO.transform.parent = transform;
+                                                    existingObjs.Add(new Vector2(i, j));
+                                                    foundSpot = true;
+                                                }
+                                        if (!foundSpot)
+                                            for (int i = 0; i < randx; i++)
+                                                for (int j = 0; j < randz; j++)
+                                                    if (!existingObjs.Contains(new Vector2(i, j)))
+                                                    {
+                                                        Vector3 spawnPos = transform.position;
+                                                        spawnPos.x += i * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                                        spawnPos.z += j * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                                        spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                                        GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                                        spawnedGO.transform.parent = transform;
+                                                        existingObjs.Add(new Vector2(i, j));
+                                                    }
+                                    }
+
+
+
+                                    else
+                                    {
+                                        Vector3 spawnPos = transform.position;
+                                        spawnPos.x += randx * (spawnerPlane.GetComponent<Renderer>().bounds.size.x + spacing / 10.0f);
+                                        spawnPos.z += randz * (spawnerPlane.GetComponent<Renderer>().bounds.size.z + spacing / 10.0f);
+                                        spawnPos.y += gameObjects[g].GetComponent<Renderer>().bounds.size.y / 2;
+                                        GameObject spawnedGO = Instantiate(gameObjects[g], spawnPos, Quaternion.identity);
+                                        spawnedGO.transform.parent = transform;
+                                        existingObjs.Add(new Vector2(randx, randz));
+
+                                    }
+                            }
+
+
+                        }   
+                    }
+
+            }
         }
     }
 }
