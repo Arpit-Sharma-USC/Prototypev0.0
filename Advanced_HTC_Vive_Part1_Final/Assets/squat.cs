4d392acc2b532404d7ea16b49058c44d32aba579
Arpit Sharma <arpitshrm07@gmail.com>
2018-10-11 00:14:45 -0700

    Squat Motion Sound added

--- Advanced_HTC_Vive_Part1_Final/Assets/squat.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/squat.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 
 public class squat : MonoBehaviour {
 
+    public AudioClip theAudioClipName;
+    private int playOnce = 0;
+    // Use this for initialization
 
-	// Use this for initialization
-	
 
     void OnTriggerEnter(Collider other)
     {
-            
-        GetComponent<AudioSource>().Play();
-            Debug.Log("Object entered the Trigger");
-            //GetComponent<AudioSource>().Play();
+        
+        //GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().playOnAwake = false;
+        
+        if(++playOnce<2)
+            GetComponent<AudioSource>().PlayOneShot(theAudioClipName,0.7f);//(theAudioClipName, transform.position);
+           
+        //GetComponent<AudioSource>().Play();
+      //  playOnce = false;
+
 
 
     }
