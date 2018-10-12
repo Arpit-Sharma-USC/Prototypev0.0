f079f53718badd7faf41b3f173483677605b77a9
rahullele <rlele@usc.edu>
2018-10-12 16:06:25 -0700

    tasks

--- Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerCollider.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerCollider.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ContainerCollider : MonoBehaviour {
+    public static int theScore;
+    private AudioSource containerAudio;
+    public AudioClip win; //set this in ispector with audiofile
+    public AudioClip loose;
+    public Text ScoreText;
 
+    private void Start()
+    {
+        theScore = 0;
+        ScoreText.text = "Score" + theScore.ToString();
+    }
     private void OnCollisionEnter(Collision collision)
     {
 
         if (collision.gameObject.name == gameObject.tag)
         {
+            theScore += 50;
+            ScoreText.text = "Score: " + theScore.ToString();
+
+            containerAudio.clip = win;
+
             Transform A = gameObject.transform;
             
             Debug.Log("Child!");
@@ -19,5 +36,10 @@ public class ContainerCollider : MonoBehaviour {
             Destroy(collision.gameObject);
             Debug.Log("Destroying after touch");
        }
+        else if(gameObject.name=="ContainerBoxParent(Clone)" && collision.gameObject.name != gameObject.tag){
+            containerAudio.clip = loose;   
+        }
+
+        containerAudio.Play();
     }
 }
