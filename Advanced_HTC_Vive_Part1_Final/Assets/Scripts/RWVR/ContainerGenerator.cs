7bb6abdc13897cb7ffd39697baeb80a1bb4cb832
Arpit Sharma <arpitshrm07@gmail.com>
2018-10-12 18:24:06 -0700

    Functioning Puzzle Mechanism: Patch
    Rigidbody fixes and setActive fixed

--- Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerGenerator.cs
+++ Advanced_HTC_Vive_Part1_Final/Assets/Scripts/RWVR/ContainerGenerator.cs
@@ -13,10 +13,12 @@ public class ContainerGenerator : MonoBehaviour {
     string inputChild;
     string myTag;
 
-    public GameObject A;
-    public GameObject B;
-    public GameObject C;
+    public GameObject A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z;
 
+    void SetContainerPhysics() {
+        this.gameObject.transform.RotateAround(transform.position, Vector3.up, -180);
+
+    }
     public void Start()
     {
         GameObject Parent = GameObject.Find("Parent");
@@ -27,7 +29,9 @@ public class ContainerGenerator : MonoBehaviour {
         //SendInputString.input = "";
 
         length = inputChild.Length;
+        SetContainerPhysics();
         Debug.Log("message recieved is"+inputChild);
+
         CreateContainers();
     }
     
@@ -45,7 +49,7 @@ public class ContainerGenerator : MonoBehaviour {
 
             Quaternion rotat = transform.rotation;
 
-            temp.z += 0.25f*(count+1);
+            temp.z -= 0.25f*(count+1);
             temp2 = temp;
 
             myTag = inputChild[count].ToString();
@@ -74,22 +78,144 @@ public class ContainerGenerator : MonoBehaviour {
                 tempObj = C;
                 flag = 1;
             }
+            else if (myTag == "D")
+            {
+                tempObj = D;
+                flag = 1;
+            }
+            else if (myTag == "E")
+            {
+                tempObj = E;
+                flag = 1;
+            }
+            else if (myTag == "F")
+            {
+                tempObj = F;
+                flag = 1;
+            }
+            else if (myTag == "G")
+            {
+                tempObj = G;
+                flag = 1;
+            }
+            else if (myTag == "H")
+            {
+                tempObj = H;
+                flag = 1;
+            }
+            else if (myTag == "I")
+            {
+                tempObj = I;
+                flag = 1;
+            }
+            else if (myTag == "J")
+            {
+                tempObj = J;
+                flag = 1;
+            }
+            else if (myTag == "K")
+            {
+                tempObj = K;
+                flag = 1;
+            }
+            else if (myTag == "L")
+            {
+                tempObj = L;
+                flag = 1;
+            }
+            else if (myTag == "M")
+            {
+                tempObj = M;
+                flag = 1;
+            }
+            else if (myTag == "N")
+            {
+                tempObj = N;
+                flag = 1;
+            }
+            else if (myTag == "O")
+            {
+                tempObj = O;
+                flag = 1;
+            }
+            else if (myTag == "P")
+            {
+                tempObj = P;
+                flag = 1;
+            }
+            else if (myTag == "Q")
+            {
+                tempObj = Q;
+                flag = 1;
+            }
+            else if (myTag == "R")
+            {
+                tempObj = R;
+                flag = 1;
+            }
+            else if (myTag == "S")
+            {
+                tempObj = S;
+                flag = 1;
+            }
+            else if (myTag == "T")
+            {
+                tempObj = T;
+                flag = 1;
+            }
+            else if (myTag == "U")
+            {
+                tempObj = U;
+                flag = 1;
+            }
+            else if (myTag == "V")
+            {
+                tempObj = V;
+                flag = 1;
+            }
+            else if (myTag == "W")
+            {
+                tempObj = W;
+                flag = 1;
+            }
+            else if (myTag == "X")
+            {
+                tempObj = X;
+                flag = 1;
+            }
+            else if (myTag == "Y")
+            {
+                tempObj = Y;
+                flag = 1;
+            }
+            else if (myTag == "Z")
+            {
+                tempObj = Z;
+                flag = 1;
+            }
+
+
 
             if (flag == 1)
             {
                 tempObj.tag = myTag;
                 tempObj.SetActive(true);
 
-                temp2.x+= 0.198f-0.092f;
-                temp2.y+= -0.121f+0.055f;
-                temp2.z+= 0.242f-0.125f;
+                //temp2.x+= 0.198f+0.092f-0.413f;
+                //temp2.y+= -0.121f+0.055f-0.062f;
+                //temp2.z+= 0.242f-0.125f+0.109f;
+                temp2.x += 0.033F;
+                temp2.y += -0.09F;
+                temp2.z += 0.164F;
 
                 //tempObj.transform.parent = container.transform;
                 // tempObj.transform.position = container.transform.position;
 
 
-                GameObject childObj = Instantiate(tempObj, temp2, rotat);
 
+
+                GameObject childObj = Instantiate(tempObj, temp2, rotat);
+                
                 childObj.transform.SetParent(foo.transform, true);
                 childObj.transform.position = temp2;
 
